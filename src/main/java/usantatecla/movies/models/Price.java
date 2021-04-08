bf47002c5f40b2d6e63b8c288c547b442a12cd1d
MRxRafi <rafadriveclase@gmail.com>
2021-04-08 18:16:58 +0200

    [Change responsability] Collapse price - Create RegularPrice constants in Price

--- src/main/java/usantatecla/movies/models/Price.java
+++ src/main/java/usantatecla/movies/models/Price.java
@@ -2,6 +2,10 @@ package usantatecla.movies.models;
 
 public abstract class Price {
 
+	private static final double CHARGE = 2.0;
+	private static final double EXTRA_CHARGE = 1.5;
+	private static final int DAYS_RENTED_THRESHOLD = 2;
+
 	private static final int FREQUENT_RENTER_POINTS = 1;
 	
 	public abstract double getCharge(int daysRented);

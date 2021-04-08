20fa78b59e3f3a8da1f52c9e0cb0f1c8e975be39
MRxRafi <rafadriveclase@gmail.com>
2021-04-08 18:26:51 +0200

    [Change responsability] Collapse price - Create NewReleasePrice constants in Price

--- src/main/java/usantatecla/movies/models/RegularPrice.java
+++ src/main/java/usantatecla/movies/models/RegularPrice.java
@@ -4,9 +4,9 @@ public class RegularPrice extends Price {
 	
 	@Override
 	public double getCharge(int daysRented) {
-		double result = Price.CHARGE;
-		if (daysRented > Price.DAYS_RENTED_THRESHOLD) {
-			result += (daysRented - Price.DAYS_RENTED_THRESHOLD) * Price.EXTRA_CHARGE;
+		double result = Price.CHARGE_REGULAR;
+		if (daysRented > Price.DAYS_RENTED_THRESHOLD_REGULAR) {
+			result += (daysRented - Price.DAYS_RENTED_THRESHOLD_REGULAR) * Price.EXTRA_CHARGE;
 		}
 		return result;
 	}

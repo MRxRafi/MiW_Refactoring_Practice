20ddbf305a666ccb298560ff7a8d90c48ef27d84
MRxRafi <rafadriveclase@gmail.com>
2021-04-08 18:20:20 +0200

    [Change responsability] Collapse price - Delete RegularPrice consts

--- src/main/java/usantatecla/movies/models/RegularPrice.java
+++ src/main/java/usantatecla/movies/models/RegularPrice.java
@@ -1,10 +1,6 @@
 package usantatecla.movies.models;
 
 public class RegularPrice extends Price {
-
-	private static final double CHARGE = 2.0;
-	private static final double EXTRA_CHARGE = 1.5;
-	private static final int DAYS_RENTED_THRESHOLD = 2;
 	
 	@Override
 	public double getCharge(int daysRented) {

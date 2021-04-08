adec5ff0e826996cecebfc153697e6ecb774e7cf
MRxRafi <rafadriveclase@gmail.com>
2021-04-08 18:19:21 +0200

    [Change responsability] Collapse price - Replace RegularPrice calls

--- src/main/java/usantatecla/movies/models/RegularPrice.java
+++ src/main/java/usantatecla/movies/models/RegularPrice.java
@@ -8,9 +8,9 @@ public class RegularPrice extends Price {
 	
 	@Override
 	public double getCharge(int daysRented) {
-		double result = RegularPrice.CHARGE;
-		if (daysRented > RegularPrice.DAYS_RENTED_THRESHOLD) {
-			result += (daysRented - RegularPrice.DAYS_RENTED_THRESHOLD) * RegularPrice.EXTRA_CHARGE;
+		double result = Price.CHARGE;
+		if (daysRented > Price.DAYS_RENTED_THRESHOLD) {
+			result += (daysRented - Price.DAYS_RENTED_THRESHOLD) * Price.EXTRA_CHARGE;
 		}
 		return result;
 	}

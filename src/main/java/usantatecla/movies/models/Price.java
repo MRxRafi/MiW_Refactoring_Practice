4a85e763c331940ec9ce8e1d2696423460a10111
MRxRafi <rafadriveclase@gmail.com>
2021-04-08 18:59:32 +0200

    [Change responsability] Collapse price - Create children prices methods in Price

--- src/main/java/usantatecla/movies/models/Price.java
+++ src/main/java/usantatecla/movies/models/Price.java
@@ -1,6 +1,6 @@
 package usantatecla.movies.models;
 
-public abstract class Price {
+public class Price {
 
 	protected static final double CHARGE_REGULAR = 2.0;
 	protected static final double EXTRA_CHARGE_REGULAR = 1.5;
@@ -16,7 +16,37 @@ public abstract class Price {
 
 	private static final int FREQUENT_RENTER_POINTS = 1;
 	
-	public abstract double getCharge(int daysRented);
+	public double getCharge(int daysRented) {
+		return 0;
+	}
+
+	public double getChargeRegular(int daysRented) {
+		double result = Price.CHARGE_REGULAR;
+		if (daysRented > Price.DAYS_RENTED_THRESHOLD_REGULAR) {
+			result += (daysRented - Price.DAYS_RENTED_THRESHOLD_REGULAR) * Price.EXTRA_CHARGE_REGULAR;
+		}
+		return result;
+	}
+
+	public double getChargeNewReleases(int daysRented) {
+		return Price.CHARGE_NEW_RELEASE;
+	}
+	
+	public int getFrequentRenterPointsNewReleases(int daysRented) {
+		if (daysRented > Price.DAYS_RENTED_THRESHOLD_NEW_RELEASE) {
+			return Price.FREQUENT_RENTER_POINTS_NEW_RELEASE;
+		} else {
+			return this.getFrequentRenterPoints(daysRented);
+		}
+	}
+
+	public double getChargeChildren(int daysRented) {
+		double result = Price.CHARGE_CHILDREN;
+		if (daysRented > Price.DAYS_RENTED_THRESHOLD_CHILDREN) {
+			result += (daysRented - 1) * Price.EXTRA_CHARGE_CHILDREN;
+		}
+		return result;
+	}
 	
 	public int getFrequentRenterPoints(int daysRented) {
 		return Price.FREQUENT_RENTER_POINTS;

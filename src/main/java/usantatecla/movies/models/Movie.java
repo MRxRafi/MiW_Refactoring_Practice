01308c7a25cbd1a081fddb936b00f1e9d1d6e38a
MRxRafi <rafadriveclase@gmail.com>
2021-04-09 10:28:57 +0200

    [Change responsability] Change responsability - Create Price constants in Movie

--- src/main/java/usantatecla/movies/models/Movie.java
+++ src/main/java/usantatecla/movies/models/Movie.java
@@ -2,6 +2,20 @@ package usantatecla.movies.models;
 
 public class Movie {
 
+	protected static final double CHARGE_REGULAR = 2.0;
+	protected static final double EXTRA_CHARGE_REGULAR = 1.5;
+	protected static final int DAYS_RENTED_THRESHOLD_REGULAR = 2;
+
+	protected static final double CHARGE_NEW_RELEASE = 3;
+	protected static final int DAYS_RENTED_THRESHOLD_NEW_RELEASE = 1;
+	protected static final int FREQUENT_RENTER_POINTS_NEW_RELEASE = 2;
+
+	protected static final double CHARGE_CHILDREN = 1.5;
+	protected static final double EXTRA_CHARGE_CHILDREN = 1.5;
+	protected static final int DAYS_RENTED_THRESHOLD_CHILDREN = 3;
+
+	private static final int FREQUENT_RENTER_POINTS = 1;
+
 	private String title;
 	private Price price;
 	private MovieType movieType;
